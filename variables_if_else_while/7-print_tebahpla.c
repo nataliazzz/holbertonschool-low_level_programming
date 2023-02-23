7cec8245e9a611a14b1f97042170d249a675381c
nataliazzz <nataliashl@gmail.com>
2023-02-22 16:52:17 -0800

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - alphabet reversed with putchar
+ *Return: always 0
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+
+	return (0);
+}
