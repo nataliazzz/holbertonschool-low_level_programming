de329d9b360235ad212a67a2b22d67093e1f0722
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-02-23 20:35:51 -0600

    at least 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * print_line - prints a line
  *
- * Return: Always 0.
+ * @n: integer to print stuff?
  */
-int main(void)
+void print_line(int n)
 {
-    print_line(0);
-    print_line(2);
-    print_line(10);
-    print_line(-4);
-    return (0);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
