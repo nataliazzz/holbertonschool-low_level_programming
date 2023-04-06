02e1424fe6a6cc2ad2940eb91913c7db934befac
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-04-05 21:08:46 -0600

    4 fix

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,33 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
-
 /**
- * free_list - free linked list
- * @head: linked list
+ * free_list - frees a list
+ * @head: head var
  */
-
 void free_list(list_t *head)
 {
+	list_t *temporary;
+
 	while (head != NULL)
 	{
+		temporary = head->next;
 		free(head->str);
 		free(head);
-		head = head->next;
+		head = temporary;
 	}
 }
-
-/**
- * another way using temp pointer
- *
- *      list_t *ptr;
- *
- *	if (head == NULL) // account for no linked list
- *		return;
- *
- *	while (head != NULL) // have ptr keep track of head node and free
- *	{
- *		ptr = head;
- *		head = head->next; // move to next node while ptr frees prior
- *		free(ptr->str); // free malloced strings
- *		free(ptr);
- *	}
- */
