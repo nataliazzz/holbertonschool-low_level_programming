392620a0f6c8e341139508477743fb4b72cfdeb7
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-04-05 21:14:43 -0600

    3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - add node to end
+ * @head: pointer to head
+ * @n: node data
+ * Return: address of new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *last;
+
+	if (!head)
+		return (NULL);
+
+	/* malloc and set new node data */
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
+		return (NULL);
+	new->n = n;
+
+	/* account for empty linked list */
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		new->prev = NULL;
+		return (new);
+	}
+
+	/* traverse to last node and insert */
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	new->next = NULL;
+	new->prev = last;
+	last->next = new;
+
+	return (new);
+}
